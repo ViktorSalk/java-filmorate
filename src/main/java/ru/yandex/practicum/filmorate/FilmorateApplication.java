5358a7bf0934ae335738fba044b58aa71fc2f5a9
Viktor64 <ecogorod64@mail.ru>
2025-01-03 00:04:37 +0400

    Убрал неиспользуемые импорты в FilmorateApplication для прохождения контроля стиля.

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,12 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-
-import java.util.Map;
-import java.util.Scanner;
-
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
